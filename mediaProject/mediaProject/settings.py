6a9344c8a24d03560928696bfd8023ebbcdbf18d
Vinayak Yande <vcharms@gmail.com>
2025-08-22 16:53:02 +0530

    Update settings.py

--- mediaProject/mediaProject/settings.py
+++ mediaProject/mediaProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yxq!)*%!f$2s88b6!o&p^=)sm1$dv!s-@*d=!@12)smi*&ryfx'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
