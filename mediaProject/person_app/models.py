675c6dce1e3b2831b3ea56889fd4aa7d2527b501
Vinayak Yande <vcharms@gmail.com>
2025-08-22 16:48:07 +0530

    Update models.py

--- mediaProject/person_app/models.py
+++ mediaProject/person_app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Person(models.Model):
-    GENDER = [
+    GENDER_CHOICES = [
         ('male', 'male'),
         ('female', 'female'),
         ('others', 'others'),
     ]
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    gender = models.CharField(max_length=20, choices=GENDER)
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
     profile_picture = models.ImageField(upload_to='profiles/')
     address = models.TextField()
     city = models.CharField(max_length=50)
